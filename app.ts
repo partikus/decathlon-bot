23a2abda67c86e64f8225d9d3a070a31161ad309
Michał Kruczek <partikus@users.noreply.github.com>
2021-02-18 14:54:12 +0000

    initial decathlon checks

--- app.ts
+++ app.ts
@@ -1,8 +1,7 @@
-import https from 'https';
-import { Telegraf } from 'telegraf';
-import cheerio from 'cheerio';
-
-function call () {}
+const https = require('https');
+const { Telegraf } = require('telegraf');
+const cheerio = require('cheerio');
+const {  CronJob } = require('cron');
 
 function loadPage (url: string): Promise<cheerio.Root|undefined> {
     return new Promise((resolve, reject) => {
@@ -13,7 +12,6 @@ function loadPage (url: string): Promise<cheerio.Root|undefined> {
                 output += d;
             });
             res.on('end', () => {
-
                 resolve(cheerio.load(output));
             });
         });
@@ -24,15 +22,54 @@ function loadPage (url: string): Promise<cheerio.Root|undefined> {
     });
 }
 
+async function checkAvailibility(url: string): Promise<{ available: boolean, url: string }> {
+    let $;
+    try {
+        $ = await loadPage(url);
+        const available = $('.stock-notification__invite--active').length === 0;
+        return {
+            available,
+            url,
+        };
+    } catch (e) {
+        console.error(e);
+    }
+    return {
+        available: false,
+        url,
+    };
+}
+
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
 
 bot.start((ctx) => ctx.reply('Welcome'))
-bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.help((ctx) => ctx.replyWithHTML(`
+Witaj w domowy robocie!
+<code>/decathlon {url}</code> - sprawdza czy dany produkt jest dostępny w sklepie decathlon.pl
+`))
 bot.on('sticker', (ctx) => ctx.reply('👍'))
 bot.hears('hi', (ctx) => ctx.reply('Hey there'))
 bot.command('hipster', Telegraf.reply('λ'))
-bot.command('oldschool', (ctx) => ctx.reply('Hello'))
+bot.command('decathlon', async (ctx) => {
+    const [cmd, url] = ctx.message.text.split(' ');
+    const { available } = await checkAvailibility(url.trim());
+
+    ctx.replyWithHTML(`Powyzszy produkt <b>${available ? 'jest' : 'nie jest'} dostępny</b>`, {
+        reply_to_message_id: ctx.message.message_id,
+    });
+});
+
+
+bot.on('inline_query', (ctx) => {
+    const result = []
+    // Explicit usage
+    ctx.telegram.answerInlineQuery(ctx.inlineQuery.id, result)
+  
+    // Using context shortcut
+    ctx.answerInlineQuery(result)
+});
+  
 bot.launch()
 
 process.once('SIGINT', () => bot.stop('SIGINT'))
